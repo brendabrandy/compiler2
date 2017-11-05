a55d50f78d52e23fb1d69fffcfdef7bbf587105b
brendabrandy <sogun3@gmail.com>
2017-11-05 00:03:29 -0400

    Finished implementing all of dominance analysis

--- dominanceAnalysis.cpp
+++ dominanceAnalysis.cpp
@@ -48,7 +48,7 @@ namespace{
                 if (i == 0){
                     ret = bb_info_mapper[bb_list[i]]->dominators;
                 }else{
-                    for (unsigned j = 0 ; j < bb_info_mapper[bb_list[i]]->dominators.size(); i++){
+                    for (unsigned j = 0 ; j < bb_info_mapper[bb_list[i]]->dominators.size(); j++){
                         if (!(ret[j] == true and bb_info_mapper[bb_list[i]]->dominators[j] == true)){
                             ret[j] = false;
                         }
@@ -64,23 +64,38 @@ namespace{
             return p_list;
         }
 
-        void printResult(BasicBlock* BB){
-            errs() << "BasicBlock : " << BB->getName() << "\n";
-            errs() << "Dominators: ";
-            for (unsigned i = 0 ; i < bb_info_mapper[BB]->dominators.size(); i++){
-                if (bb_info_mapper[BB]->dominators[i] == false){
+        void printBitVector(std::vector<bool> b){
+            for (unsigned i = 0 ; i < b.size(); i++){
+                if (b[i] == false){
                     errs() << "0";
                 }else{
                     errs() << "1";
                 }
             }
+
+        }
+
+        void printResult(BasicBlock* BB){
+            errs() << "BasicBlock : " << BB->getName() << "\n";
+            errs() << "Dominators: ";
+            printBitVector(bb_info_mapper[BB]->dominators);
             errs() << "\n";
+            errs() <<  "Strict Dominators: ";
+            printBitVector(bb_info_mapper[BB]->strict_dominators);
+            errs() << "\n";
+            errs() <<  "Inverse Dominators: ";
+            printBitVector(bb_info_mapper[BB]->inverse_dominators);
+            errs() << "\n";
+            errs() <<  "Immediate Dominators: ";
+            printBitVector(bb_info_mapper[BB]->immediate_dominators);
+            errs() << "\n";
+
         }
         
         bool runOnFunction(Function &F) override{
 
             unsigned counter;
-            bool isChanged;
+            bool isChanged, isImmDom;
             std::vector<bool> new_dom;
             std::vector<BasicBlock*> p_list;
             std::vector<bool> p_i;  // boolean to keep track of intersection of predecessors
@@ -105,6 +120,8 @@ namespace{
                 // set dom(BB) = whole set of basic blocks
                 for (unsigned i = 0 ; i< counter; i++){
                     bb_info_mapper[BB]->dominators.push_back(true);
+                    bb_info_mapper[BB]->inverse_dominators.push_back(false);
+                    bb_info_mapper[BB]->immediate_dominators.push_back(false);
                 }
                 if (BB == &(F.getEntryBlock())){
                     // set dom(entry) = entry only
@@ -139,6 +156,62 @@ namespace{
                 }
             }
 
+            // find strict dominators
+            for (Function::iterator b = F.begin(); b != F.end(); b++){
+                BasicBlock *BB = &(*b);
+                bb_info_mapper[BB]->strict_dominators = bb_info_mapper[BB]->dominators;
+                bb_info_mapper[BB]->strict_dominators[value_mapper[BB]] = false;
+            }
+
+            // find immediate dominators
+            // the definition of immediate dominators is that it is the closest dominator to d
+            // meaning that other dominators that dominate d also dominate the immediate dominator
+            // it is proven that every block has a unique immediate dominator
+            for (Function::iterator b = F.begin(); b !=  F.end(); b ++){
+                BasicBlock *BB = &(*b);
+                // loop through strict dominators
+                if (BB != &(F.getEntryBlock())){
+                    p_list.clear();     // use this list to save dominators
+                    
+                    for (Function::iterator b2 =F.begin(); b2 != F.end(); b2++){
+                        BasicBlock *BB2 = &(*b2);
+                        if (bb_info_mapper[BB]->strict_dominators[value_mapper[BB2]] == true){
+                            p_list.push_back(BB2);
+                        }
+                    }
+                    
+                    for (unsigned i = 0 ; i < p_list.size(); i++){
+                        isImmDom = true;
+                        for (unsigned j = 0 ; j < p_list.size(); j++){
+                            if (i != j){
+                                // check whether p_list[j] is a strict dominator of p_list[i]
+                                if (bb_info_mapper[p_list[i]]->
+                                    strict_dominators[value_mapper[p_list[j]]] != true){
+                                // if not, then p_list[i] is not an immediate dominator of BB
+                                    isImmDom = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (isImmDom){
+                            bb_info_mapper[BB]->immediate_dominators[value_mapper[p_list[i]]] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // find inverse dominators
+            for (Function::iterator b = F.begin(); b != F.end(); b++){
+                BasicBlock *curr_bb = &(*b);
+                for (Function::iterator b2 = F.begin(); b2 != F.end(); b2++){
+                    BasicBlock *other_bb = &(*b);
+                    if (bb_info_mapper[other_bb]->dominators[value_mapper[curr_bb]] == true){
+                        bb_info_mapper[curr_bb]->inverse_dominators[value_mapper[other_bb]] = true;
+                    }
+                }
+            }
+
             // print out result
             for (Function::iterator b = F.begin(); b != F.end(); b++){
                 BasicBlock *BB = &(*b);
@@ -149,3 +222,7 @@ namespace{
         }
     };
 }
+
+
+char bso_dominance_analysis::ID = 0;
+static RegisterPass<bso_dominance_analysis> D("bso_dominance_analysis","BSO: Iterative Algorithm for Dominance Analysis");
